0dfea80f7fd5cf3a21a5e01ad488dd6c1690879d
Made-sudiarta <>
2026-04-29 01:28:20 +0800

    kembali

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,8 +47,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return true;
-    }
 }
